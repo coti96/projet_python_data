f7436b462ae13dbd83ec2f4535d417268083941f
paluchc <100384497+coti96@users.noreply.github.com>
2023-12-28 15:05:00 +0100

    Recommencement

--- data.py
+++ data.py
@@ -3,12 +3,6 @@ from dash import Dash, dcc, html, dash_table
 from dash.dependencies import Input, Output
 from dash.dash_table.Format import Group
 
-#########################################
-import plotly.express as px
-from dash import Dash, dcc, html
-from dash.dependencies import Input, Output
-############################################
-
 # 1. Créer une instance de l'application Dash
 app = Dash(__name__)
 
@@ -16,10 +10,14 @@ app = Dash(__name__)
 url = "https://data.economie.gouv.fr/explore/dataset/prix-des-carburants-en-france-flux-instantane-v2/download?format=csv&timezone=Europe/Berlin&use_labels_for_header=false"
 df = pd.read_csv(url, sep=';')  # je spécifie le séparateur
 
+
+
 # 3. Sélectionner uniquement les colonnes souhaitées
 df = df.loc[:, ['id', 'cp', 'adresse', 'ville', 'geom', 'gazole_maj', 'gazole_prix', 'sp95_maj', 'sp95_prix', 'departement', 'code_departement', 'region', 'code_region']]
 
 # 4. Mise en forme des données sous forme de tableau
+
+
 app.layout = html.Div([
     html.H1('Prix des Carburants en France'),
     dash_table.DataTable(
@@ -40,42 +38,3 @@ app.layout = html.Div([
 #5. Exécuter l'application
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-###############################################################################
-
-# Créer un dataframe à partir d'un fichier csv
-df = pd.read_csv('data.csv')
-
-# Définir les colonnes pour la comparaison
-df = df[['id', 'nom', 'prenom', 'age', 'region', 'code_region']]
-
-# Créer un graphique interactif avec Plotly
-fig = px.scatter(df, x="age", y="region", color="code_region")
-
-# Ajouter le graphique à l'application Dash
-app.layout = html.Div([
-    dcc.Graph(figure=fig),
-    html.Label('Choisissez une région'),
-    dcc.Dropdown(
-        id='region-dropdown',
-        options=[{'label': i, 'value': i} for i in df['region'].unique()],
-        value='Toutes les régions'
-    ),
-    dcc.Graph(id='output-graph')
-])
-
-@app.callback(
-    Output('output-graph', 'figure'),
-    [Input('region-dropdown', 'value')]
-)
-def update_graph(region):
-    if region == 'Toutes les régions':
-        return px.scatter(df, x="age", y="region", color="code_region")
-    else:
-        df_filtered = df[df['region'] == region]
-        return px.scatter(df_filtered, x="age", y="region", color="code_region")
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
-################################################################################
