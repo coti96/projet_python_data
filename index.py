36844c7d1951af18461de39f2352ce73e5b0f7cc
ngoo <olivier.ngo@edu.esiee.fr>
2024-01-03 16:58:53 +0100

    Ajout du style css

--- index.py
+++ index.py
@@ -7,6 +7,8 @@ from components.map import prepare_coordinates, plot_gas_stations
 # Créer l'instance de l'application Dash
 app = create_app()
 
+app.css.append_css({'external_url': '/assets/style.css'})
+
 # Créer  l'histogramme
 fig_histogram = create_histogram()
 
@@ -15,12 +17,37 @@ fig_map = plot_gas_stations()
 
 # Définir l'agencement de l'application
 app.layout = html.Div(children=[
+    html.H1("Dashboard Carburants en France", 
+            style={
+                'textAlign': 'center', 
+                'color': '#ffffff',
+                'font-family': 'Roboto, sans-serif',  # Choisir la police de ton choix
+                'font-size': '36px',  # Ajuster la taille de la police
+                'font-weight': 'bold',  # Rendre le texte en gras si nécessaire
+            }
+    ),  # Ajout du titre
     html.Div([
-        dcc.Graph(figure=fig_histogram),
-    ], style={'width': '50%', 'display': 'inline-block'}),  # Histogramme à gauche
+        dcc.Graph(
+            figure=fig_histogram,
+            style={
+                'backgroundColor': '#4242b6',
+                'border': '2px solid #92959B',
+                'padding': '10px',
+                'border-radius': '10px',
+            }
+        ),
+    ], style={'width': '45%', 'display': 'inline-block', 'margin': '20px 0 20px 60px'}),  # Histogramme à gauche
     html.Div([
-        dcc.Graph(figure=fig_map),
-    ], style={'width': '50%', 'display': 'inline-block'}),  # Carte à droite
+        dcc.Graph(
+            figure=fig_map,
+            style={
+                'backgroundColor': '#4242b6',
+                'border': '2px solid #92959B',
+                'padding': '10px',
+                'border-radius': '10px',
+            }
+        ),
+    ], style={'width': '45%', 'display': 'inline-block', 'margin': '20px 60px 20px 0'}),  # Carte à droite
 ])
 
 
